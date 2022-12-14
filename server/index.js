f72b2a5ef41d414ef4bb133ad569d9309733d466
Ahnaf-dev <ahnafbari99@gmail.com>
2022-12-14 12:22:25 -0700

    connecting to mongoDB

--- server/index.js
+++ server/index.js
@@ -3,9 +3,11 @@ require("dotenv").config();
 const { graphqlHTTP } = require("express-graphql");
 const cors = require("cors");
 const port = process.env.PORT || 5000;
+const connectDB = require("./config/db");
 const path = require("path");
 const app = express();
 
+connectDB();
 app.use(cors());
 
 app.listen(port, console.log(`Server running on ${port}`));
