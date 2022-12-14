96b8c9e2353d1a2f19ea8ff2aad26765575db7b5
Ahnaf-dev <ahnafbari99@gmail.com>
2022-12-14 13:12:43 -0700

    finishing backend with graphql queries and mutations bounded to graphql endpoint

--- server/index.js
+++ server/index.js
@@ -3,11 +3,20 @@ require("dotenv").config();
 const { graphqlHTTP } = require("express-graphql");
 const cors = require("cors");
 const port = process.env.PORT || 5000;
+const schema = require("./schema/schema");
 const connectDB = require("./config/db");
 const path = require("path");
 const app = express();
 
 connectDB();
 app.use(cors());
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    graphiql: process.env.NODE_ENV === "development",
+  })
+);
 
 app.listen(port, console.log(`Server running on ${port}`));
--- server/schema/schema.js
+++ server/schema/schema.js
@@ -19,3 +19,41 @@ const QuestionType = new GraphQLObjectType({
     correct: { type: GraphQLInt },
   }),
 });
+
+const RootQuery = new GraphQLObjectType({
+  name: "RootQueryType",
+  fields: {
+    questions: {
+      type: new GraphQLList(QuestionType),
+      resolve(parent, args) {
+        return Questions.find();
+      },
+    },
+  },
+});
+
+const mutation = new GraphQLObjectType({
+  name: "Mutation",
+  fields: {
+    addQuestion: {
+      type: QuestionType,
+      args: {
+        question: { type: GraphQLString },
+        answers: { type: new GraphQLList(GraphQLString) },
+        correct: { type: GraphQLInt },
+      },
+      resolve(parent, args) {
+        return Questions.create({
+          question: args.question,
+          answers: args.answers,
+          correct: args.correct,
+        });
+      },
+    },
+  },
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery,
+  mutation,
+});
